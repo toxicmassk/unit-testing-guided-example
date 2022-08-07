12e8280901acf20027efdbe548c8aa37dc266fb9
Julia Lipniewicz <julialipniewicz@yahoo.de>
2022-08-07 17:32:54 +0200

    almost solved

--- calculator.js
+++ calculator.js
@@ -1,14 +1,30 @@
 function sum(a, b) {
-  return;
+  if (a === undefined || b === undefined) {
+   return 0; 
+  } else if (a && b === undefined) {
+    return 0;
+  } else {
+    return a + b;
+  }
 }
 
 function subtract(a, b) {
-  return;
+  if (a === undefined || b === undefined) {
+    return 0;
+  } else if (a && b === undefined) {
+    return 0;
+  } else {
+    return a - b; 
+  }
 }
 
 function divide(a, b) {
-  return;
-}
+  if (a === 0 || b === 0) {
+   throw new Error('Cannot divide by 0.');
+  } else {
+   return a / b;
+  }
+};
 
 function multiply(a, b) {
   return a * b;
